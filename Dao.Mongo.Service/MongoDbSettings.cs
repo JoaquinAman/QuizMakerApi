c3ae65f6fe18050e2d6a66fc4479d1de35cbaf35
joaquin <joaco.aman@gmail.com>
2022-10-20 23:37:40 -0300

    second commit

--- Dao.Mongo.Service/MongoDbSettings.cs
+++ Dao.Mongo.Service/MongoDbSettings.cs
@@ -4,8 +4,6 @@ namespace Dao.Mongo.Service
 {
     public class MongoDbSettings : IMongoDbSettings
     {
-        //public string ConnectionString { get { return "mongodb+srv://diego:contrasena@videoform.4ctniww.mongodb.net/?retryWrites=true&w=majority"; } set { } }
-        //public string DatabaseName { get { return "videoForm"; } set { } }
         public string ConnectionString { get; set; } = null!;
         public string DatabaseName { get; set; } = null!;
     }
